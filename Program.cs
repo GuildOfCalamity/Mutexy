350efdc964827ed35bbe3b7faa060de567d5882b
GuildOfCalamity <81915293+GuildOfCalamity@users.noreply.github.com>
2024-09-18 16:44:21 -0400

    clean-up

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
-﻿namespace Mutexy;
+﻿using System.Threading;
+
+namespace Mutexy;
 
 internal class Program
 {
-    static int maxThreads = 10;
+    static int maxThreads = 6;
+    static ValueStopwatch vsw = ValueStopwatch.StartNew();
+
     static void Main(string[] args)
     {
         Console.OutputEncoding = Encoding.UTF8;
+        if (args.Length > 0)
+        {
+            if (Int32.TryParse(args[0], out int tmp))
+                maxThreads = tmp;
+        }
         ThreadSafeResource resource = new ThreadSafeResource();
         Thread[] _workers = new Thread[maxThreads];
         for (int i = 0; i < maxThreads; i++)
@@ -15,17 +24,19 @@ internal class Program
             {
                 while (!resource.AccessResource($"Thread #{tnum}"))
                 {
+                    var sleep = Random.Shared.Next(2000,5000);
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"⇨ Thread #{tnum} failed to access the resource, trying again...");
+                    Console.WriteLine($"⇨ Thread #{tnum} failed to access the resource, trying again in {new TimeSpan(0, 0, 0, 0, sleep).ToReadableString()}...");
                     Console.WriteLine($"⇨ Thread resource is currently \"{resource.Property}\"");
                     Console.ForegroundColor = ConsoleColor.Gray;
-                    Thread.Sleep(2000);
+                    Thread.Sleep(sleep);
                 }
             })
             { IsBackground = true }).Start();
         }
         for (int i = 0; i < maxThreads; i++) { _workers[i].Join(); }
         Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"⇨ Test took {vsw.GetElapsedTime().TotalSeconds:N1} seconds");
         Console.WriteLine($"⇨ Test completed. Press any key to exit.");
         Console.ForegroundColor = ConsoleColor.Gray;
         _ = Console.ReadKey(true);
--- ThreadSafeResource.cs
+++ ThreadSafeResource.cs
@@ -2,8 +2,19 @@
 
 public class ThreadSafeResource
 {
-    readonly int _timeout = 1000;
+    readonly int _waitTimeout = 1000;
     readonly Mutex _mutex = new Mutex(false, @"Global\ImTooMutexy");
+
+    /// <summary>
+    /// For performance/optimization reasons, field memory locations may be 
+    /// rearranged for certain kinds of reads/writes. Fields that are declared 
+    /// volatile are excluded from certain kinds of optimizations.
+    /// On a multiprocessor system, a volatile read operation does not guarantee 
+    /// to obtain the latest value written to that memory location by any processor. 
+    /// Similarly, a volatile write operation does not guarantee that the value 
+    /// written would be immediately visible to other processors.
+    /// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/volatile
+    /// </summary>
     volatile string? _property;
     public string? Property
     {
@@ -11,18 +22,22 @@ public class ThreadSafeResource
         set => _property = value;
     }
 
-    // Method that simulates a critical section of code.
+    /// <summary>
+    /// Method to simulate shared access of a resource.
+    /// </summary>
+    /// <param name="threadName">the name of the thread requesting access</param>
+    /// <returns>true if access granted, false if access blocked</returns>
     public bool AccessResource(string threadName)
     {
-        Console.WriteLine($"⇒ {threadName} is requesting the resource (I'll only wait for {new TimeSpan(0, 0, 0, 0, _timeout).ToReadableString()})");
-        bool lockAcquired = _mutex.WaitOne(_timeout);
+        Console.WriteLine($"⇒ {threadName} is requesting the resource (I'll only wait for {new TimeSpan(0, 0, 0, 0, _waitTimeout).ToReadableString()})");
+        bool lockAcquired = _mutex.WaitOne(_waitTimeout);
         if (lockAcquired)
         {
             try
             {
                 var sleep = Random.Shared.Next(4000);
                 Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine($"⇒ {threadName} has entered the critical section ({sleep}ms)");
+                Console.WriteLine($"⇒ {threadName} has entered the critical section and will remain for {new TimeSpan(0, 0, 0, 0, sleep).ToReadableString()}");
                 Console.ForegroundColor = ConsoleColor.Gray;
 
                 // Simulate some work with the shared resource.
@@ -30,7 +45,7 @@ public class ThreadSafeResource
                 Thread.Sleep(sleep);
 
                 Console.ForegroundColor = ConsoleColor.DarkCyan;
-                Console.WriteLine($"⇒ {threadName} is leaving the critical section ({sleep}ms)");
+                Console.WriteLine($"⇒ {threadName} is leaving the critical section");
                 Console.ForegroundColor = ConsoleColor.Gray;
             }
             finally
